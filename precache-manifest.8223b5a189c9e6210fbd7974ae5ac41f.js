7e87c193137ebb11811f31be40483bf01df23b4d
vahe-khachatryan <vahe.khachatryann@gmail.com>
2020-10-23 00:16:58 +0400

    Updates

---
+++ precache-manifest.8223b5a189c9e6210fbd7974ae5ac41f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73364d90d651be9031b45de0780d96c6",
+    "url": "/receipe_app/index.html"
+  },
+  {
+    "revision": "5f95b4f312fa5c737b5b",
+    "url": "/receipe_app/static/css/main.98995b4d.chunk.css"
+  },
+  {
+    "revision": "b55528c3b7cee675ae25",
+    "url": "/receipe_app/static/js/2.81bf5c5d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/receipe_app/static/js/2.81bf5c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f95b4f312fa5c737b5b",
+    "url": "/receipe_app/static/js/main.a43203a9.chunk.js"
+  },
+  {
+    "revision": "09cab1b353efac98a11c",
+    "url": "/receipe_app/static/js/runtime-main.a0f79781.js"
+  }
+]);
